54d380d891d2439611fb520de5a059a4bf65129c
Auralcat <ashleyzt@hi2.in>
2017-08-24 21:59:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#RO Goodies
+
+These are programs intended to make life easier in the game Ragnarok
+Online.
+
+Current programs:
+
+- Helper intended to track what you sell and how much things cost in the
+  game.
---
+++ sell_tracker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""This is a program to show you what you sold in the vending shop when you
+   log back in the game"""
+
+if __name__ == '__main__':
+    # Main method goes here
+    print("Welcome to the market tracker!")
+    print("""Please tell me which items you are vending right now.
+             You can vend up to 12 items in your personal shop.""")
+    vending_items = dict()
+    buf = None
+    while buf != "":
+        buf = input("Please type the item name (press Enter to skip): ")
+        if buf == "":
+            break
+        else:
+            price = input("Please type the item price: ")
+            amount = input("Please tell me how many you are selling: ")
+            vending_items[buf] = (amount, price)
+
+    print("Here are the items you informed me about:\n", vending_items)
